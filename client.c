ca152389465bc2293369f20d925de2947f59edd7
Mert Akengin <mert.akengin@iyzico.com>
2019-09-08 15:10:36 +0300

    add shell to client module

--- client.c
+++ client.c
@@ -4,10 +4,13 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
+#include <sys/ioctl.h>
 #include <mysql/mysql.h>
 
 #define MAX(x, y) ( ((x) > (y)) ? (x) : (y) )
+
 
 int
 print(FILE *fd, MYSQL *m, const char *fmt, ...) {
@@ -34,12 +37,26 @@ void tableprint(MYSQL_RES *result, long rows) {
 	if(!result) {
 		return;
 	}
+	struct winsize termsz;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &termsz);
+	unsigned long width = (termsz.ws_col ? termsz.ws_col : 80);
 	for(long i=0, n=mysql_num_fields(result); i<mysql_num_rows(result); i++) {
-		printf("row: ^");
 		MYSQL_ROW row = mysql_fetch_row(result);
-		unsigned long *l=mysql_fetch_lengths(result);
+		unsigned long *ln = mysql_fetch_lengths(result);
+		MYSQL_FIELD *fields = mysql_fetch_fields(result);
+		unsigned long colwidth = width/n - 2*n - 8;
+		if(!i) {
+			printf("head: ^");
+			for(int j=0; j<n; j++) {
+				//MYSQL_FIELD *field = mysql_fetch_field(result);
+				printf("[%*s]", -MAX(colwidth, ln[j]), fields[j].name);
+				continue;
+			}
+			printf("$\n");
+		}
+		printf("_row: ^");
 		for(int j=0; j<n; j++) {
-			printf("[%*s]", -MAX(80UL/n, l[j]), row[j]?row[j]:"NULL");
+			printf("[%*s]", -MAX(colwidth, ln[j]), row[j]?row[j]:"NULL");
 			continue;
 		}
 		printf("$\n");
@@ -50,32 +67,65 @@ void tableprint(MYSQL_RES *result, long rows) {
 }
 
 int
-tester(const char *host, unsigned short port, const char *db, const char *user, const char *pass, const char **commands) {
+tester(
+	const char *host,
+	unsigned short port,
+	const char *db,
+	const char *user,
+	const char *pass,
+	const char **commands
+) {
 	MYSQL *con = mysql_init(NULL);
-	mysql_options(con, MYSQL_INIT_COMMAND, (void *)"show processlist;");
+	//mysql_options(con, MYSQL_INIT_COMMAND, (void *)"show processlist;");
 	mysql_real_connect(con, host, user, pass, db, port, NULL, 0);
-	if(print(stdout, con, "isrv: %s", mysql_get_server_info(con))) goto exithandler;
-	if(print(stdout, con, "prot: %d", mysql_get_proto_info(con)))  goto exithandler;
-	if(print(stdout, con, "host: %s", mysql_get_host_info(con)))   goto exithandler;
-	if(print(stdout, con, "icli: %s", mysql_get_client_info()))    goto exithandler;
-	if(print(stdout, con, "stat: %s", mysql_stat(con)))            goto exithandler;
-	if(print(stdout, con, "info: %s", mysql_info(con)))            goto exithandler;
+	if(print(stdout, con, "isrv: %s", mysql_get_server_info(con))) goto quit;
+	if(print(stdout, con, "prot: %d", mysql_get_proto_info(con)))  goto quit;
+	if(print(stdout, con, "host: %s", mysql_get_host_info(con)))   goto quit;
+	if(print(stdout, con, "icli: %s", mysql_get_client_info()))    goto quit;
+	if(print(stdout, con, "stat: %s", mysql_stat(con)))            goto quit;
+	if(print(stdout, con, "info: %s", mysql_info(con)))            goto quit;
 	tableprint(mysql_list_processes(con), 0);
 	tableprint(mysql_list_dbs(con, NULL), 0);
 	tableprint(mysql_list_tables(con, NULL), 0);
 	while(commands && *commands) {
+		if('-' == **commands) {
+			const size_t length = 999;
+			FILE *in = stdin;
+			while(!feof(in)) {
+				char *buffer = (char*) calloc(length, sizeof(char));
+				printf("sql> ");
+				getline(&buffer, &length, in);
+				//buffer[abs(strlen(buffer)-1)] = 0x00;
+				//printf("dbg-sql: %s\n", buffer);
+				if(!buffer || !*buffer || !strlen(buffer))
+					break;
+				mysql_query(con, buffer);
+				tableprint(
+					mysql_store_result(con), 
+					mysql_affected_rows(con)
+				);
+				error(con);
+				free(buffer);
+				continue;
+			}
+			goto next;
+		}
 		print(stdout, con, "exec: %s", *commands);
 		if(print(stdout, con, "query: %d", mysql_query(con, *commands)))
-			goto exithandler;
+			goto quit;
 		tableprint(mysql_store_result(con), mysql_affected_rows(con));
-		commands += 1;
+		next: {
+			commands += 1;
+			continue;
+		}
+		goto next;
 	}
-	exithandler: {
+	quit: {
 		int err = error(con);
 		mysql_close(con);
 		return err;
 	}
-	return 0;
+	goto quit;
 }
 
 int
--- client.dockerfile
+++ client.dockerfile
@@ -12,11 +12,9 @@ RUN cc -g -c      -o client.o client.c
 RUN cc -g -static -o client   client.c \
 	-lmysqlclient -lpthread -lstdc++ -lm -lz -ldl
 ENTRYPOINT [ "gdb", "--args", "client" ]
-CMD        [ "host.docker.internal", "3306", "test", "user", "pass" ]
+CMD        [ "host.docker.internal", "3306", "test", "user", "pass", "-#" ]
 
-FROM debian:latest
-#RUN apt update
-#RUN apt install -y libmysqlclient20
+FROM debian:sid
 COPY --from=builder /data/client /bin/mysqltest
 ENTRYPOINT [ "mysqltest" ]
 CMD        [ "host.docker.internal", "3306", "test", "user", "pass" ]
